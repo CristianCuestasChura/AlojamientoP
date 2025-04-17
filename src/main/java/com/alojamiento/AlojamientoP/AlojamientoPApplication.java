0517684b4aa3b07680ba329f1f75af145c766c44
CristianCuestasChura <crisscuestass@gmail.com>
2025-04-16 23:37:27 -0300

    documento descargado de spring initilizr

---
+++ src/main/java/com/alojamiento/AlojamientoP/AlojamientoPApplication.java
@@ -0,0 +1,13 @@
+package com.alojamiento.AlojamientoP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlojamientoPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlojamientoPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alojamiento/AlojamientoP/AlojamientoPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alojamiento.AlojamientoP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlojamientoPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
